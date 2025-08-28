f376b06afa4eec235c9cc92bbf979535aa78d571
Timothy Cope <kirbycope@gmail.com>
2025-08-27 22:41:36 -0700

    wip

--- addons/3d_player_controller/states/hanging.gd
+++ addons/3d_player_controller/states/hanging.gd
@@ -50,6 +50,8 @@ func _input(event: InputEvent) -> void:
 
 ## Called every frame. '_delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	# Uncomment the next line if using GodotSteam
+	if !is_multiplayer_authority(): return
 	# Check if the player is "hanging"
 	if player.is_hanging:
 		# Check if the player has no raycast collision
--- addons/3d_player_controller/states/swimming.gd
+++ addons/3d_player_controller/states/swimming.gd
@@ -12,6 +12,8 @@ const SWIMMING_ENTRY_LERP_DURATION: float = 0.08
 
 ## Called every frame. '_delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	# Uncomment the next line if using GodotSteam
+	if !is_multiplayer_authority(): return
 	# Check if the game is not paused
 	if !player.game_paused:
 		# Smooth entry into water
