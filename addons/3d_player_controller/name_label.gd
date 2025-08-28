9b818e936fdc65e9809950a398db355d5339be0c
Timothy Cope <kirbycope@gmail.com>
2025-08-28 15:08:40 -0700

    wip

--- addons/3d_player_controller/name_label.gd
+++ addons/3d_player_controller/name_label.gd
@@ -3,5 +3,7 @@ extends Label3D
 
 ## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Set playername
-	text = str(Steam.getPersonaName())
+	# Set the username for this user only
+	if is_multiplayer_authority():
+		# Set playername
+		text = str(Steam.getPersonaName())
