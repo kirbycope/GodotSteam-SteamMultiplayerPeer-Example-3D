0d0069a5b4ce6164dafa7778ecd21ea3887f38ce
Timothy Cope <kirbycope@gmail.com>
2025-08-28 18:20:53 -0700

    Update main.gd

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -8,14 +8,18 @@ var lobby_members: Array = []
 var lobby_members_max: int = 10
 var lobby_vote_kick: bool = false
 var steam_id: int = 0
-var steam_username: String = ""
 
 # https://michaelmacha.wordpress.com/2024/04/08/godotsteam-and-steammultiplayerpeer/
+const CONNECTION_TIMEOUT: float = 10.0
 var peer: SteamMultiplayerPeer
-@onready var spawner: MultiplayerSpawner = $MultiplayerSpawner
 var pending_owner_id: int = 0
 var connection_timeout_timer: Timer = null
-const CONNECTION_TIMEOUT: float = 10.0
+@onready var spawner: MultiplayerSpawner = $MultiplayerSpawner
+
+## Prints the console message if in debug mode.
+func _debug_print(message: String) -> void:
+	if OS.is_debug_build():
+		print("[LOG] %s" % message)
 
 
 # https://godotsteam.com/tutorials/lobbies/#the-_ready-function
@@ -29,16 +33,11 @@ func _ready() -> void:
 	Steam.lobby_match_list.connect(_on_lobby_match_list)
 	#Steam.lobby_message.connect(_on_lobby_message)
 	Steam.persona_state_change.connect(_on_persona_change)
-
 	multiplayer.connection_failed.connect(_on_connection_failed)
 	multiplayer.connected_to_server.connect(_on_connection_success)
-
-	# Check for command line arguments
-	# Cache our Steam ID and persona name (used when creating/closing P2P sessions)
+	# Cache SteamID for logged in user
 	steam_id = Steam.getSteamID()
-	steam_username = Steam.getPersonaName()
-	print("Local Steam ID: %s, name: %s" % [steam_id, steam_username])
-
+	# Check for command line arguments
 	check_command_line()
 	# Define custom spawner
 	spawner.spawn_function = spawn_level
@@ -52,14 +51,13 @@ func _process(_delta: float) -> void:
 	var current_peer = multiplayer.get_multiplayer_peer()
 	if current_peer != null:
 		# Check if the owner is still connected
-		# Check if the owner is still connected
 		if lobby_id > 0 and not multiplayer.is_server():
 			var owner_id = Steam.getLobbyOwner(lobby_id)
 			if owner_id != steam_id:  # We're not the owner
 				# Check if we're actually disconnected (not just connecting)
 				var peer_state = current_peer.get_connection_status()
 				if peer_state == MultiplayerPeer.CONNECTION_DISCONNECTED:
-					print("Multiplayer peer is no longer active. Disconnecting gracefully...")
+					_debug_print("Multiplayer peer is no longer active. Disconnecting gracefully...")
 					# Leave the lobby on Steam
 					leave_lobby()
 					# Clean up multiplayer peer
@@ -73,21 +71,6 @@ func _process(_delta: float) -> void:
 					pending_owner_id = 0
 
 
-func _on_connection_success():
-	print("Connection success!")
-	# Stop any connection timeout timer
-	_stop_connection_timeout()
-
-
-func _on_connection_failed():
-	print("Connection failed.")
-	# Cleanup any pending connection attempt
-	if pending_owner_id != 0:
-		Steam.closeP2PSessionWithUser(pending_owner_id)
-		pending_owner_id = 0
-	_stop_connection_timeout()
-
-
 # https://godotsteam.com/tutorials/lobbies/#the-_ready-function
 func check_command_line() -> void:
 	var these_arguments: Array = OS.get_cmdline_args()
@@ -99,7 +82,7 @@ func check_command_line() -> void:
 			if int(these_arguments[1]) > 0:
 				# At this point, you'll probably want to change scenes
 				# Something like a loading into lobby screen
-				print("Command line lobby ID: %s" % these_arguments[1])
+				_debug_print("Command line lobby ID: %s" % these_arguments[1])
 				join_lobby(int(these_arguments[1]))
 
 
@@ -128,7 +111,7 @@ func _on_lobby_created(connection: int, this_lobby_id: int) -> void:
 	if connection == 1:
 		# Set the lobby ID
 		lobby_id = this_lobby_id
-		print("Created a lobby: %s" % lobby_id)
+		_debug_print("Created a lobby: %s" % lobby_id)
 		# Set this lobby as joinable, just in case, though this should be done by default
 		Steam.setLobbyJoinable(lobby_id, true)
 		# Set some lobby data
@@ -136,14 +119,14 @@ func _on_lobby_created(connection: int, this_lobby_id: int) -> void:
 		Steam.setLobbyData(lobby_id, "mode", "GodotSteam test")
 		# Allow P2P connections to fallback to being relayed through Steam if needed
 		#var set_relay: bool = Steam.allowP2PPacketRelay(true)
-		#print("Allowing Steam to be relay backup: %s" % set_relay)
+		#_debug_print("Allowing Steam to be relay backup: %s" % set_relay)
 
 
 # https://godotsteam.com/tutorials/lobbies/#get-lobby-lists
 func _on_open_lobby_list_pressed() -> void:
 	# Set distance to worldwide
 	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
-	print("Requesting a lobby list...")
+	_debug_print("Requesting a lobby list...")
 	Steam.requestLobbyList()
 
 
@@ -165,12 +148,12 @@ func _on_lobby_match_list(lobbies: Array) -> void:
 		lobby_button.connect("pressed", Callable(self, "join_lobby").bind(lobby))
 		# Add the new lobby to the list
 		$GUI/LobbyContainer/Lobbies.add_child(lobby_button)
-	print("Lobby list updated.")
+	_debug_print("Lobby list updated.")
 
 
 # https://godotsteam.com/tutorials/lobbies/#joining-lobbies
 func join_lobby(this_lobby_id: int) -> void:
-	print("Attempting to join lobby %s" % this_lobby_id)
+	_debug_print("Attempting to join lobby %s" % this_lobby_id)
 	# Clear any previous lobby members lists, if you were in a previous lobby
 	lobby_members.clear()
 	# Make the lobby join request to Steam
@@ -195,7 +178,7 @@ func _on_lobby_joined(this_lobby_id: int, _permissions: int, _locked: bool, resp
 		Steam.CHAT_ROOM_ENTER_RESPONSE_MEMBER_BLOCKED_YOU: response_str = "CHAT_ROOM_ENTER_RESPONSE_MEMBER_BLOCKED_YOU"
 		Steam.CHAT_ROOM_ENTER_RESPONSE_YOU_BLOCKED_MEMBER: response_str = "CHAT_ROOM_ENTER_RESPONSE_YOU_BLOCKED_MEMBER"
 		_: response_str = "UNKNOWN_RESPONSE(%s)" % response
-	print("Joined lobby %s with response code %s (%s)" % [this_lobby_id, response, response_str])
+	_debug_print("Joined lobby %s with response code %s (%s)" % [this_lobby_id, response, response_str])
 	# If joining was successful
 	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
 		# Set this lobby ID as your lobby ID
@@ -214,7 +197,7 @@ func _on_lobby_joined(this_lobby_id: int, _permissions: int, _locked: bool, resp
 			# Use the owner's Steam ID so we attempt to connect to them
 			peer.create_client(owner_id, 0)
 			multiplayer.set_multiplayer_peer(peer)
-			print("Created Steam client to owner %s; waiting for connection...." % owner_id)
+			_debug_print("Created Steam client to owner %s; waiting for connection...." % owner_id)
 			# Start a timeout in case the P2P connection never completes
 			_start_connection_timeout(owner_id)
 
@@ -233,7 +216,7 @@ func _on_lobby_joined(this_lobby_id: int, _permissions: int, _locked: bool, resp
 			Steam.CHAT_ROOM_ENTER_RESPONSE_COMMUNITY_BAN: fail_reason = "This lobby is community locked."
 			Steam.CHAT_ROOM_ENTER_RESPONSE_MEMBER_BLOCKED_YOU: fail_reason = "A user in the lobby has blocked you from joining."
 			Steam.CHAT_ROOM_ENTER_RESPONSE_YOU_BLOCKED_MEMBER: fail_reason = "A user you have blocked is in the lobby."
-		print("Failed to join this chat room: %s" % fail_reason)
+		_debug_print("Failed to join this chat room: %s" % fail_reason)
 		# Reopen the lobby list
 		_on_open_lobby_list_pressed()
 
@@ -242,7 +225,7 @@ func _on_lobby_joined(this_lobby_id: int, _permissions: int, _locked: bool, resp
 func _on_lobby_join_requested(this_lobby_id: int, friend_id: int) -> void:
 	# Get the lobby owner's name
 	var owner_name: String = Steam.getFriendPersonaName(friend_id)
-	print("Joining %s's lobby..." % owner_name)
+	_debug_print("Joining %s's lobby..." % owner_name)
 	# Attempt to join the lobby
 	join_lobby(this_lobby_id)
 
@@ -261,21 +244,21 @@ func get_lobby_members() -> void:
 		var member_steam_name: String = Steam.getFriendPersonaName(member_steam_id)
 		# Add them to the list
 		lobby_members.append({"steam_id": member_steam_id, "steam_name": member_steam_name})
-	print("Current lobby members: %s" % [lobby_members])
+	_debug_print("Current lobby members: %s" % [lobby_members])
 
 
 # https://godotsteam.com/tutorials/lobbies/#persona-changes-avatars-names
 func _on_persona_change(this_steam_id: int, _flag: int) -> void:
 	# Make sure you're in a lobby and this user is valid or Steam might spam your console log
 	if lobby_id > 0:
-		print("A user (%s) had information change, updating the lobby list..." % this_steam_id)
+		_debug_print("A user (%s) had information change, updating the lobby list..." % this_steam_id)
 		# Update the player list
 		get_lobby_members()
 
 
 # https://godotsteam.com/tutorials/lobbies/#p2p-handshakes
 func make_p2p_handshake() -> void:
-	print("Sending P2P handshake to the lobby")
+	_debug_print("Sending P2P handshake to the lobby")
 	#send_p2p_packet(0, {"message": "handshake", "from": steam_id})
 
 
@@ -285,19 +268,19 @@ func _on_lobby_chat_update(_this_lobby_id: int, change_id: int, _making_change_i
 	var changer_name: String = Steam.getFriendPersonaName(change_id)
 	# If a player has joined the lobby
 	if chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_ENTERED:
-		print("%s has joined the lobby." % changer_name)
+		_debug_print("%s has joined the lobby." % changer_name)
 	# Else if a player has left the lobby
 	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_LEFT:
-		print("%s has left the lobby." % changer_name)
+		_debug_print("%s has left the lobby." % changer_name)
 	# Else if a player has been kicked
 	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_KICKED:
-		print("%s has been kicked from the lobby." % changer_name)
+		_debug_print("%s has been kicked from the lobby." % changer_name)
 	# Else if a player has been banned
 	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_BANNED:
-		print("%s has been banned from the lobby." % changer_name)
+		_debug_print("%s has been banned from the lobby." % changer_name)
 	# Else there was some unknown change
 	else:
-		print("%s did... something." % changer_name)
+		_debug_print("%s did... something." % changer_name)
 	# Update the lobby now that a change has occurred
 	get_lobby_members()
 
@@ -312,7 +295,7 @@ func _on_send_chat_pressed() -> void:
 		var was_sent: bool = Steam.sendLobbyChatMsg(lobby_id, this_message)
 		# Was it sent successfully?
 		if not was_sent:
-			print("ERROR: Chat message failed to send.")
+			_debug_print("ERROR: Chat message failed to send.")
 	# Clear the chat input
 	$Chat.clear()
 
@@ -354,7 +337,7 @@ func _start_connection_timeout(owner_id: int) -> void:
 		add_child(connection_timeout_timer)
 	# Start/restart the timer
 	connection_timeout_timer.start()
-	print("Started connection timeout (%ss) for owner %s" % [CONNECTION_TIMEOUT, owner_id])
+	_debug_print("Started connection timeout (%ss) for owner %s" % [CONNECTION_TIMEOUT, owner_id])
 
 
 func _stop_connection_timeout() -> void:
@@ -365,8 +348,23 @@ func _stop_connection_timeout() -> void:
 	pending_owner_id = 0
 
 
+func _on_connection_failed():
+	_debug_print("Connection failed.")
+	# Cleanup any pending connection attempt
+	if pending_owner_id != 0:
+		Steam.closeP2PSessionWithUser(pending_owner_id)
+		pending_owner_id = 0
+	_stop_connection_timeout()
+
+
+func _on_connection_success():
+	_debug_print("Connection success!")
+	# Stop any connection timeout timer
+	_stop_connection_timeout()
+
+
 func _on_connection_timeout() -> void:
-	print("Connection to owner %s timed out after %s seconds." % [pending_owner_id, CONNECTION_TIMEOUT])
+	_debug_print("Connection to owner %s timed out after %s seconds." % [pending_owner_id, CONNECTION_TIMEOUT])
 	# Close any open P2P session to the owner
 	if pending_owner_id != 0:
 		Steam.closeP2PSessionWithUser(pending_owner_id)
