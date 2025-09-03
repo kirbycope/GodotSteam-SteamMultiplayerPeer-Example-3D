92a0c847dae19d6655e9c15c1f837a19a1e4bc50
Timothy Cope <kirbycope@gmail.com>
2025-09-02 23:51:22 -0700

    Update steam_lobby.gd

--- scenes/main/steam_lobby.gd
+++ scenes/main/steam_lobby.gd
@@ -381,6 +381,7 @@ func read_p2p_voice_packet():
 			var players_in_scene: Array = get_tree().get_nodes_in_group("players")
 			for player in players_in_scene:
 				print("player.steam_id: %s" % player.steam_id)
+				print("player.steam_username: %s" % player.steam_username)
 				if player.steam_id == packet_sender:
 					player.process_voice_data(readable_data, "network")
 				else:
